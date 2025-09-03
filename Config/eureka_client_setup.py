186182689d4166a169b355fb1333f243d0c47f11
Édgar Julián González Sierra <ejgonzalez@javeriana.edu.co>
2025-09-02 22:21:08 -0500

    Registro en Eureka completo

---
+++ Config/eureka_client_setup.py
@@ -0,0 +1,14 @@
+from py_eureka_client import eureka_client
+
+# Dirección del servidor Eureka
+EUREKA_SERVER = "http://localhost:8761/eureka/"
+
+# Registrar el microservicio
+eureka_client.init(
+    eureka_server=EUREKA_SERVER,
+    app_name="llm-service",      # Nombre de tu servicio
+    instance_port=8000,        # Puerto de Django
+    instance_host="localhost", # Host de la instancia
+    renewal_interval_in_secs=10,
+    duration_in_secs=30,
+)
--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import Config.eureka_client_setup
 
 
 def main():
