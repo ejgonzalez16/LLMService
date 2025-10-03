f4d02d0ddd4a295d11c53e75afd40f8ae47495e7
Édgar Julián González Sierra <ejgonzalez@javeriana.edu.co>
2025-10-03 02:28:16 -0500

    ip fixed

--- Config/eureka_client_setup.py
+++ Config/eureka_client_setup.py
@@ -8,7 +8,7 @@ eureka_client.init(
     eureka_server=EUREKA_SERVER,
     app_name="llm-service",      # Nombre de tu servicio
     instance_port=8001,        # Puerto de Django
-    instance_host="localhost", # Host de la instancia
+    instance_host="hpe-service", # Host de la instancia
     renewal_interval_in_secs=10,
     duration_in_secs=30,
 )
