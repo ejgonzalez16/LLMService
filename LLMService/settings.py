84cfb40309c8f383693199e99044e148d0dc9605
Édgar Julián González Sierra <ejgonzalez@javeriana.edu.co>
2025-10-03 01:44:02 -0500

    urls names solved

--- LLMService/settings.py
+++ LLMService/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Controller.PromptController'
+    'Controller.PromptController',
     'Controller.PreferenciasController'
 ]
 
