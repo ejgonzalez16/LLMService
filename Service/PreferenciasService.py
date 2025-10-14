5b471c7f4a2ee450f2c8fb7da00f8011f8a76776
Édgar Julián González Sierra <ejgonzalez@javeriana.edu.co>
2025-10-13 19:49:24 -0500

    IP
    2

--- Service/PreferenciasService.py
+++ Service/PreferenciasService.py
@@ -33,7 +33,7 @@ def insertarPreferencias(idUsuario, prescripciones):
         }
     }
 
-    resp = requests.post(f"http://10.101.136.233:11434/api/chat", json=payload)
+    resp = requests.post(f"http://10.101.137.210:11434/api/chat", json=payload)
     print(resp.json()['message']['content'])
 
     try:
